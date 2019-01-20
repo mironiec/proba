41432daeffa33709c5a934cb90c15d96b2aba409
Miroslaw <miroslaw.nieckarz@gmail.com>
2019-01-20 10:28:59 +0100

    zmiana class

--- src/main/java/Struktury.java
+++ src/main/java/Struktury.java
@@ -1,28 +1,15 @@
+
 public class Struktury {
 
 
     public static void main(String[] args) {
         int[] tab = new int[10];
         for (int i = 0; i < 9; i++) tab[i] = i+1;
-        for (int i = tab.length - 1; i >= 0; i--) {
-            if (i % 2 == 0) System.out.println(tab[i]);
-        }
 
-        int suma = 0;
+        tablice.piszParzyste(tab);
 
-        for (int i = 0; i < tab.length; i++) {
-            if (tab[i] % 3 == 0) suma = suma + tab[i];
-        }
-        System.out.println(suma);
-        suma = 0;
+        tablice.podzielnePrzezTrzy(tab);
 
-        for (int i = 0; i<5; i++) {
-            suma = suma + tab[i];
-            if (i==4) suma = suma - tab[5];
-
-        }
-
-
-        System.out.println(suma);
+        tablice.sumaPieciuMinusSzesc(tab);
     }
 }
---
+++ src/main/java/tablice.java
@@ -0,0 +1,31 @@
+public class tablice {
+
+    public static void piszParzyste (int[] tab) {
+        for (int i = tab.length - 1; i >= 0; i--) {
+            if (i % 2 == 0) System.out.println(tab[i]);
+        }
+    }
+
+    public static void podzielnePrzezTrzy(int[] tab) {
+        int suma = 0;
+
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] % 3 == 0) suma = suma + tab[i];
+        }
+        System.out.println(suma);
+    }
+
+    public static void sumaPieciuMinusSzesc(int[] tab) {
+        int suma = 0;
+
+        for (int i = 0; i<5; i++) {
+            suma = suma + tab[i];
+            if (i==4) suma = suma - tab[5];
+
+        }
+
+
+        System.out.println(suma);
+    }
+
+}
