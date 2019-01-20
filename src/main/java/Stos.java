727505a4c56424eae4ae6d3a0ed7f6fafbce79d4
Miroslaw <miroslaw.nieckarz@gmail.com>
2019-01-20 10:57:07 +0100

    zmiana class

---
+++ src/main/java/Stos.java
@@ -0,0 +1,23 @@
+public class Stos {
+
+    private int[] tab;
+
+    private int top = -1;
+
+    public  StosTab(int size) {
+        tab = new int[size];
+    }
+
+    public void push(int value) {
+        tab[++top] = value;
+    }
+
+    public int pop() {
+        // int value = tab[top
+        // top--;
+        // return value;
+        return tab[top--];
+    }
+    public int
+
+}
