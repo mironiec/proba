555471e99c0feb3af5277f527f0fa7256334aa28
Miroslaw <miroslaw.nieckarz@gmail.com>
2019-01-20 10:08:16 +0100

    proba

--- src/main/java/Struktury.java
+++ src/main/java/Struktury.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Struktury {
+
+
+    public static void main(String[] args) {
+        int[] tab = new int[10];
+        for (int i = 0; i < 9; i++) tab[i] = i+1;
+        for (int i = tab.length - 1; i >= 0; i--) {
+            if (i % 2 == 0) System.out.println(tab[i]);
+        }
+
+        int suma = 0;
+
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] % 3 == 0) suma = suma + tab[i];
+        }
+        System.out.println(suma);
+        suma = 0;
+
+        for (int i = 0; i<5; i++) {
+            suma = suma + tab[i];
+            if (i==4) suma = suma - tab[5];
+
+        }
+
+
+        System.out.println(suma);
+    }
 }
